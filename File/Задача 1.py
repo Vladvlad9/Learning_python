a179d5600b955357a0c928c316b3d8ba29a50eda
Vlad Paulechka <vlad9.ru@gmail.com>
2022-01-24 16:21:28 +0300

    learning first task

--- File/Задача 1.py
+++ File/Задача 1.py
@@ -1,4 +1,12 @@
-with open('index.txt', 'r', encoding='utf-8') as txt:
-    line = [i.strip().strip() for i in txt]
-    a = line[-2]
-    print(a)
+"""№1 в файле хранятся числа, каждое на своей строке, и не только числа, необходимо просуммировать все числа,
+игнорируя строки где не числа или не только числа"""
+
+numbers = 0
+with open('index.txt', 'r', encoding='utf-8') as file:
+    lst = [line.strip() for line in file]
+
+    for j in lst:
+        if j.isnumeric():
+            numbers += int(j)
+
+    print('Сумма всех чисел равна: ' + str(numbers))
